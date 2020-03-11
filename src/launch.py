456f34f354d31123796d1f6d60daece4b914a0f6
ismasgrove <45636192+ismasgrove@users.noreply.github.com>
2020-03-11 03:21:44 +0200

    Update launch.py

--- src/launch.py
+++ src/launch.py
@@ -7,8 +7,14 @@ import select, os
 
 while (True):
     #scan fdisk entries after enabling automount on fstab
-    #or monitor proc/mounts using poll
+    #or monitor proc/mounts <- the best solution as Raspbian automounts drives
+    #   by default in an arbitrary directory
     #or use 'watchdog' pip module
+
+    f = open('/proc/self/mounts')
+    pollobj = select.poll() #A 'polling object'
+    pollobj(f, select.POLLERR | select.POLLPRI)
+    pollobj.poll(None) #needs testing
 
     #if a drive new drive is detected, then
         #ensure that it's mounted properly and is working
