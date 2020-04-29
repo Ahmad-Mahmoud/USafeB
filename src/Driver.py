725983186bef06e095fac8f4a61a31b825a5e67e
ismasgrove <45636192+ismasgrove@users.noreply.github.com>
2020-04-29 04:20:55 +0200

    added driver.py

---
+++ src/Driver.py
@@ -0,0 +1,21 @@
+
+
+
+def main():
+    while (True):
+        #Starting to monitor the new USB devices entering and extract the BUS number and the mounted path
+        usbInfo = get_external_data()
+        #Extract the serial number from the entered device
+        serial = dataExtract(SERIAL)
+        #Check if the device is already blacklistetd or not
+        BlackListCheck(serial)
+        #Monitor the descriptor packets sent from the USB and Host and detect anomalies
+        monitor(usbInfo[0])
+        #Check for all binary files and scan them
+        vtscan(usbInfo[1])
+        #IF malicious
+        if (malicious == true):
+            BlackListAdd(serial)
+if __name__ == "__main__":
+    main()
+
