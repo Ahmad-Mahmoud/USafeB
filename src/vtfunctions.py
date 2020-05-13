1b8b85ac42f0333a4a53efacfbeaacd4187d8c46
RaniaKAlieldeen <48138421+RaniaKAlieldeen@users.noreply.github.com>
2020-05-13 03:54:19 +0200

    vtscan returns "files_to_encrypt" : list

--- src/vtfunctions.py
+++ src/vtfunctions.py
@@ -3,37 +3,37 @@ from isExecFile import isExecFile
 
 key = ''
 reports = []
+files_to_encrypt = []
 
 def vtscan(directory) : 
     
     execFiles = isExecFile(directory)
-    for x in execFiles:
-        if not x:
-            continue
-        file_to_scan = x[0]
+    if execFiles:
+        for x in execFiles:
+           file_to_scan = x[0]
+            
+            if(x[1]<(3.2e+7)) : #file size limit in virustotal
         
-        if(x[1]<(3.2e+7)) : #file size limit in virustotal
+                url = 'https://www.virustotal.com/vtapi/v2/file/scan'
+                params = {'apikey': key}
+                f iles = {'file': (file_to_scan, open(file_to_scan, 'rb'))}
+                response = requests.post(url, files=files, params=params)
+                #print(response.json())
     
-            url = 'https://www.virustotal.com/vtapi/v2/file/scan'
-            params = {'apikey': key}
-            files = {'file': (file_to_scan, open(file_to_scan, 'rb'))}
-            response = requests.post(url, files=files, params=params)
-            #print(response.json())
-
-        else : #in case of files larger than 32MB
-            #restricted api: must contact 
-            url = 'https://www.virustotal.com/vtapi/v2/file/scan/upload_url'
-            params = {'apikey':key}
-            response = requests.get(url, params=params)
-            upload_url = response.json()['upload_url']
-            
-            
-            files = {'file': (file_to_scan, open(file_to_scan, 'rb'))}
-            response = requests.post(upload_url, files=files)
-            
-        md5_resource = response.json()['md5']
-        vtreport(md5_resource, file_to_scan)
-
+            else : #in case of files larger than 32MB
+                #restricted api: must contact 
+                url = 'https://www.virustotal.com/vtapi/v2/file/scan/upload_url'
+                params = {'apikey':key}
+                response = requests.get(url, params=params)
+                upload_url = response.json()['upload_url']
+                
+                
+                files = {'file': (file_to_scan, open(file_to_scan, 'rb'))}
+                response = requests.post(upload_url, files=files)
+                
+            md5_resource = response.json()['md5']
+            vtreport(md5_resource,file_to_scan)
+    return files_to_encrypt
 
 #%%
 
@@ -43,11 +43,7 @@ def vtreport(resource,file_to_scan):
     params = {'apikey': key , 'resource': resource }
     response = requests.get(url, params=params)
     result_positives = response.json()['positives']
-    if result_positives < 5: #if the positives are less than 5 then it is condemended as safe
-        #print("No malicious software detected")
-        return False # not a malicious software
-    else:
-        #print("Malicious software detected: " + file_to_scan)
-        return True # malicious software detected
+    if result_positives >= 5: #if the positives are greater than or equal 5 then it is condemended as unsafe
+        files_to_encrypt.append(file_to_scan)
 
 #%%
